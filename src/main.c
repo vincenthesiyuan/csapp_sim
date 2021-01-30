4f2fdd9ef5c7b395254760062da23aa5d7545484
vincenthesiyuan <vincenthesiyuan@gmail.com>
2021-01-30 15:24:37 +0800

    update main

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 #include<stdint.h>
 
 #include "cpu/register.h"
+#include "cpu/mmu.h"
 #include "memory/instruction.h"
 #include "disk/code.c"
+#include "memory/dram.h"
 
 int main()
 {
@@ -20,19 +22,63 @@ int main()
 
     reg.rip = (uint64_t)&program[11];
 
-    
+    mm[va2pa(0x7fffffffdae4)] = 0x00005555;
+    mm[va2pa(0x7fffffffdae0)] = 0x55554630;   // rbp
+    mm[va2pa(0x7fffffffdad8)] = 0x00000000;
+    mm[va2pa(0x7fffffffdad0)] = 0xabcd0000;
+    mm[va2pa(0x7fffffffdac8)] = 0x00001234;
+    mm[va2pa(0x7fffffffdac4)] = 0x00005555;
+    mm[va2pa(0x7fffffffdac0)] = 0x55554630;   // rsp
+
 
     // run inst
-    int i = 0;
-    for(; i<15; i++)
+    for(int i = 0; i<15; i++)
     {
-        
+        instruction_cycle();
     }
 
 
-    // verify
+    // register verify
+    // 验证程序运行完后的register上的值和memory值是否与gdb上的值一致
+    int match = 1;
 
+    match = match && (reg.rax == 0xabcd1234);
+    match = match && (reg.rbx == 0x0);
+    match = match && (reg.rcx == 0x555555554630);
+    match = match && (reg.rdx == 0x1234);
+    match = match && (reg.rsi == 0xabcd0000);
+    match = match && (reg.rdi == 0x1234);
+    match = match && (reg.rbp == 0x7fffffffdae0);
+    match = match && (reg.rsp == 0x7fffffffdac0);
 
+    if(match == 1)
+    {
+        printf("register match! \n");
+    }
+    else
+    {
+        printf("register not match! \n");
+    }
+    
+    // memory verify
+    match = 1;
+
+    match = match && (mm[va2pa(0x7fffffffdae4)] == 0x00005555);
+    match = match && (mm[va2pa(0x7fffffffdae0)] == 0x55554630);   // rbp
+    match = match && (mm[va2pa(0x7fffffffdad8)] == 0xabcd1234);
+    match = match && (mm[va2pa(0x7fffffffdad0)] == 0xabcd0000);
+    match = match && (mm[va2pa(0x7fffffffdac8)] == 0x00001234);
+    match = match && (mm[va2pa(0x7fffffffdac4)] == 0x00005555);
+    match = match && (mm[va2pa(0x7fffffffdac0)] == 0x55554630);   // rsp
+
+    if(match == 1)
+    {
+        printf("memory match! \n");
+    }
+    else
+    {
+        printf("memory not match! \n");
+    }
 
     return 0;
 }
--- src/memory/dram.h
+++ src/memory/dram.h
@@ -3,11 +3,15 @@
 
 #include<stdint.h>
 
-#define MM_LEN 1000;
+#define MM_LEN 1000
+
+uint8_t mm[MM_LEN];
 
 void write64bits_dram(uint64_t paddr, uint64_t data);
 
 uint64_t read64bits_dram(uint64_t paddr);
+
+
 
 
 #endif
